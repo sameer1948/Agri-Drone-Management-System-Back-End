8959e08421b7f576e447110a4a57fa6a4345f55f
Sameer <sameer.sameer185@gmail.com>
2026-09-19 22:34:02 +0530

    Initial commit

---
+++ src/test/java/com/agri_drone_management_system_back_end/AgriDroneManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agri_drone_management_system_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriDroneManagementSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
